99743a372d22495f04f6f6b3c848a09050679dba
fdipuccio <32079226+fdipuccio@users.noreply.github.com>
2026-02-19 18:51:33 +0100

    Add CartRepository interface

---
+++ src/main/java/com/example/ecommerce/repository/CartRepository.java
@@ -0,0 +1,6 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface CartRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByUser(User user);
+}
