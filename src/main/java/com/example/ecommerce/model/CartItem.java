0c3df8e1fe68762f9ddf88e41980ed66b85b6857
fdipuccio <32079226+fdipuccio@users.noreply.github.com>
2026-02-19 18:51:19 +0100

    Add CartItem model class

---
+++ src/main/java/com/example/ecommerce/model/CartItem.java
@@ -0,0 +1,21 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+
+    // Getters and Setters
+}
