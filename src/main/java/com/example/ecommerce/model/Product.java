f774e90307f08ff4c88b57a33ac3e36c15c2a5da
fdipuccio <32079226+fdipuccio@users.noreply.github.com>
2026-02-19 18:51:15 +0100

    Add Product model class

---
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -0,0 +1,22 @@
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 100)
+    private String name;
+
+    private String description;
+
+    @NotNull
+    private Double price;
+
+    // Getters and Setters
+}
